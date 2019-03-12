8c4884cc61f810276576c858d19a6ac2fe461202
rtanubra <rdtanubrata@gmail.com>
2019-03-12 07:31:39 -0400

    completing final commit

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 "use strict";
 const base_url = "https://api.github.com"
 
+function appendRepoRender(repoName,repoLink){
+    $(".js-Results-ul").append(`
+    <li>${repoName} <span><strong>link at:</strong> <a target="_blank" href ="${repoLink}">${repoLink}</a></span></li>
+    `)
+}
+
 function jsonIterator(jsonObj){
     console.log("iterating through object")
     for (let i = 0; i < jsonObj.length; i++){
         const gitRepoName = jsonObj[i].name
         const gitRepoLink = jsonObj[i].html_url
+        appendRepoRender(gitRepoName,gitRepoLink)
     }
 }
 function emptyRequest(){
     $(".js-Results-ul").empty()
 }
+
+function mainRunner(jsonObj,user){
+    $(".js-userName").text(`See below ${user} repos:`)
+    emptyRequest();
+    jsonIterator(jsonObj)
+}
+
 function fetchUser(user){
     const options = {
     headers: new Headers({
       "Accept": "application/vnd.github.v3+json"})};
     const full_url = base_url+`/users/${user}/repos`
     fetch(full_url,options).then(response=>{
+        //display error or success message
         $(".results-page").removeClass("hidden")
         if (response.ok){
             return response.json();
         } 
+        emptyRequest();
         throw new Error(response.statusText);
     }).then(responseJson=>{
-        empty
-        jsonIterator(responseJson)
-        $(".js-userName").text(`See below ${user} repos:`)
+        mainRunner(responseJson,user)
     }).catch(err=>
         $(".js-userName").text(err));
 }//End of fetchUser function
